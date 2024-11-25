d29813264cbcdf235dacf6d184673267f7b9c901
RonexLemon <ronexondimu@gmail.com>
2024-11-25 23:49:35 +0300

    prediction route

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import predicition from "./src/routes/prediction"
 const App = express()
 const port =  process.env.PORT as unknown as number || 3000
 App.use(express.json())
 App.use(express.urlencoded({ extended: true }))
+App.use("/prediction", predicition)
 
 
     App.listen(port, () => {
